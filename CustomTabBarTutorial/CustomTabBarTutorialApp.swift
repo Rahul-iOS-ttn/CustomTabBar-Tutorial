7b1a1464c4721c7eecf49d95ca7f4a18fab38afa
Rahul Sharma <rahul.sharma1@tothenew.com>
2021-12-22 14:50:27 +0530

    Initial Commit

---
+++ CustomTabBarTutorial/CustomTabBarTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarTutorialApp.swift
+//  CustomTabBarTutorial
+//
+//  Created by TTN on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
