c439b211cbd64af436e59a4f567ddea8d82b4fcd
Rahul Sharma <rahul.sharma1@tothenew.com>
2021-12-23 12:00:54 +0530

    - Hiding the tab bar on navigating to a view.

--- CustomTabBarTutorial/ContentView.swift
+++ CustomTabBarTutorial/ContentView.swift
@@ -9,28 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            Text("Home")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
+        NavigationView {
+            TabView {
+                List(1...10, id: \.self) { index in
+                    
+                    NavigationLink(
+                        destination: Text(" \(index)s Detail"),
+                        label: {
+                            Text("\(index)'s Detail Page").font(
+                                .system(size: 18, weight: .semibold, design: .rounded))
+                        })
+                    
+                }.tabItem {
                     Image(systemName: "house.fill")
                     Text("Home")
-                } // this is your tab item that corresponds to your view.
-            Text("Bookmark")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "bookmark.circle.fill")
-                    Text("Bookmark")
                 }
-            Text("profile")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                    Text("profile")
-                }
-        }.onAppear() {
-            UITabBar.appearance().barTintColor = .white
-        }.accentColor(.yellow)// this is a tab Veiw
+                Text("Bookmark")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "bookmark.circle.fill")
+                        Text("Bookmark")
+                    }
+                Text("profile")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "person.crop.circle")
+                        Text("profile")
+                    }
+            }.onAppear() {
+                UITabBar.appearance().barTintColor = .white
+            }.accentColor(.yellow)
+        }.navigationTitle("TabViewDemo")
     }
 }
 
