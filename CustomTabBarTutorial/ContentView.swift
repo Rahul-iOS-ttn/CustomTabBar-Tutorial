971a1d9ebbdb5a01af73192f23e9ca625ca15ef9
Rahul Sharma <rahul.sharma1@tothenew.com>
2021-12-23 10:25:48 +0530

    - Made tab bar using tabView

--- CustomTabBarTutorial/ContentView.swift
+++ CustomTabBarTutorial/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Home")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                } // this is your tab item that corresponds to your view.
+            Text("Bookmark")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "bookmark.circle.fill")
+                    Text("Home")
+                }
+            Text("profile")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Home")
+                }
+        }// this is a tab Veiw
     }
 }
 
