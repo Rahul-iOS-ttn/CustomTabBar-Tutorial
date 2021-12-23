40dcc44de9b5477639149a7146e3f209327b881e
Rahul Sharma <rahul.sharma1@tothenew.com>
2021-12-23 12:40:00 +0530

    Reversed the hierarchy for Navigation and TabView in order to display the tab bar even inside a detail page.

--- CustomTabBarTutorial/ContentView.swift
+++ CustomTabBarTutorial/ContentView.swift
@@ -9,37 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            TabView {
+        TabView {
+            NavigationView {
                 List(1...10, id: \.self) { index in
-                    
                     NavigationLink(
-                        destination: Text(" \(index)s Detail"),
+                        destination: Text("Item #\(index) Details"),
                         label: {
-                            Text("\(index)'s Detail Page").font(
-                                .system(size: 18, weight: .semibold, design: .rounded))
+                            Text("Item #\(index)")
+                                .font(.system(size: 20, weight: .bold, design: .rounded))
                         })
                     
-                }.tabItem {
-                    Image(systemName: "house.fill")
-                    Text("Home")
                 }
-                Text("Bookmark")
-                    .font(.system(size: 30, weight: .bold, design: .rounded))
-                    .tabItem {
-                        Image(systemName: "bookmark.circle.fill")
-                        Text("Bookmark")
-                    }
-                Text("profile")
-                    .font(.system(size: 30, weight: .bold, design: .rounded))
-                    .tabItem {
-                        Image(systemName: "person.crop.circle")
-                        Text("profile")
-                    }
-            }.onAppear() {
-                UITabBar.appearance().barTintColor = .white
-            }.accentColor(.yellow)
-        }.navigationTitle("TabViewDemo")
+                
+                .navigationTitle("TabView Demo")
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            Text("Bookmark")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "bookmark.circle.fill")
+                    Text("Bookmark")
+                }
+            Text("profile")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("profile")
+                }
+        }
     }
 }
 
